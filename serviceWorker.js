259b98c1296ea1f6cb21579fdcaa2be00630bfc2
zero2one01 <55082315+zero2one01@users.noreply.github.com>
2025-02-03 22:38:03 +0800

    1

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,15 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const PWA = "pwa-version1";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
   "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(PWA).then(cache => {
       cache.addAll(assets);
     })
   );
